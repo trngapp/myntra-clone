303b1076a8293e45c896a9a8ad5fb4acfd28029d
trngapp <74776793+trngapp@users.noreply.github.com>
2022-07-08 23:18:46 +0530

    added build

--- src/components/Product/ImageModal.js
+++ src/components/Product/ImageModal.js
@@ -1,4 +1,4 @@
-import React,{useEffect} from "react";
+import React  from "react";
 import "./ImageModal.css"
 const Modal=(props)=>{
 if(!props.show )
@@ -7,11 +7,11 @@ if(!props.show )
 }
 
     return (
-        <>
+<>
 <div className="modal" onClick={props.onClose}>
     <div className="modal-content" onClick={e=>e.stopPropagation()}>
         <div className="modal-header">
-         <h2 className="modal-title"></h2>
+
         </div>
         <div className="modal-body">
       <img src={props.src} className="ImageModal" alt="img"/>
